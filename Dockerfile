c87c711e4688406d0eb6dee98456a8211345fd38
Dieter Reuter <dieter.reuter@me.com>
2015-12-12 21:55:04 +0100

    Add debootstrap script

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM debian:jessie
 RUN apt-get update
-RUN apt-get install -y wget
+RUN apt-get install -y \
+    qemu \
+    qemu-user-static \
+    binfmt-support \
+    debootstrap \
+    debian-ports-archive-keyring
 
 RUN mkdir /data
 WORKDIR /data
 COPY build.sh /
-
-RUN /build.sh
--- build.sh
+++ build.sh
@@ -1,3 +1,22 @@
 #!/bin/bash -e
+set -x
 
-touch rootfs.tar.gz
+#touch rootfs.tar.gz
+
+# Debootstrap a minimal Debian Jessie rootfs 
+qemu-debootstrap \
+  --arch=arm64 \
+  --keyring /usr/share/keyrings/debian-archive-keyring.gpg 
+  --variant=buildd \
+  --exclude=debfoster \
+  --include=net-tools \
+  jessie \
+  debian-arm64 \
+  http://ftp.debian.org/debian
+
+# set root user
+chroot debian-arm64 \
+   echo "root:shield" | chpasswd
+
+# Package tarball
+tar -czf /data/rootfs.tar.gz -C debian-arm64/ .   
