4ffc60b0ce541d70d102cade180d8aade134054e
Dieter Reuter <dieter.reuter@me.com>
2015-12-13 12:55:27 +0100

    Support for 32-bit and 64-bit builds

--- build.sh
+++ build.sh
@@ -1,36 +1,41 @@
 #!/bin/bash -e
 set -x
 
+# Build Debian rootfs for ARCH={armhf,arm64}
+# - Debian armhf = ARMv6/ARMv7
+# - Debian arm64 = ARMv8/Aarch64
+BUILD_ARCH=${BUILD_ARCH:-arm64}
+ROOTFS_DIR=debian-${BUILD_ARCH}
+
 # Cleanup
-rm -fr debian-arm64
+rm -fr ${ROOTFS_DIR}
 
 # Debootstrap a minimal Debian Jessie rootfs 
 #  --keyring /usr/share/keyrings/debian-ports-archive-keyring.gpg \
 #  --no-check-gpg \
 #  --variant=buildd \
 qemu-debootstrap \
-  --arch=arm64 \
+  --arch=${BUILD_ARCH} \
   --exclude=debfoster \
   --include=net-tools \
   jessie \
-  debian-arm64 \
+  ${ROOTFS_DIR} \
   http://ftp.debian.org/debian
 
 ### HypriotOS specific settings ###
 
 # set hostname to 'black-pearl'
-echo 'black-pearl' | sudo chroot debian-arm64 tee /etc/hostname
+echo 'black-pearl' | sudo chroot ${ROOTFS_DIR} tee /etc/hostname
 
 # set root password to 'hypriot'
-echo 'root:hypriot' | sudo chroot debian-arm64 /usr/sbin/chpasswd
+echo 'root:hypriot' | sudo chroot ${ROOTFS_DIR} /usr/sbin/chpasswd
 
 # set ethernet interface eth0 to dhcp
-cat << EOM | sudo chroot debian-arm64 tee /etc/network/interfaces.d/eth0.cfg
+cat << EOM | sudo chroot ${ROOTFS_DIR} tee /etc/network/interfaces.d/eth0.cfg
 # The network interface
 auto eth0
 iface eth0 inet dhcp
 EOM
 
-
 # Package rootfs tarball
-tar -czf rootfs.tar.gz -C debian-arm64/ .
+tar -czf rootfs-${BUILD_ARCH}.tar.gz -C ${ROOTFS_DIR}/ .
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+# Install build dependencies
 sudo apt-get update
 sudo apt-get install -y \
     qemu \
@@ -9,5 +10,10 @@ sudo apt-get install -y \
     debian-ports-archive-keyring \
     --no-install-recommends
 
-sudo /vagrant/build.sh
-cp rootfs.tar.gz /vagrant/rootfs.tar.gz
+# Build for armhf (32-bit ARMv6, ARMv7)
+sudo BUILD_ARCH=armhf /vagrant/build.sh
+cp rootfs-armhf.tar.gz /vagrant/rootfs-armhf.tar.gz
+
+# Build for arm64 (64-bit ARMv8, Aarch64)
+sudo BUILD_ARCH=arm64 /vagrant/build.sh
+cp rootfs-arm64.tar.gz /vagrant/rootfs-arm64.tar.gz
