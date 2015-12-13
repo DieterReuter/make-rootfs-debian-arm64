739e7e7e72a360549823d99e9d49593a548082a2
Dieter Reuter <dieter.reuter@me.com>
2015-12-13 01:50:35 +0100

    Configure eth0 network interface

--- build.sh
+++ build.sh
@@ -24,6 +24,13 @@ echo 'black-pearl' | sudo chroot debian-arm64 tee /etc/hostname
 # set root password to 'hypriot'
 echo 'root:hypriot' | sudo chroot debian-arm64 /usr/sbin/chpasswd
 
+# set ethernet interface eth0 to dhcp
+cat << EOM | sudo chroot debian-arm64 tee /etc/network/interfaces.d/eth0.cfg
+# The network interface
+auto eth0
+iface eth0 inet dhcp
+EOM
+
 
 # Package rootfs tarball
 tar -czf rootfs.tar.gz -C debian-arm64/ .
