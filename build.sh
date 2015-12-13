40f903e489a026e2f98320af9276faa59075c3f3
Dieter Reuter <dieter.reuter@me.com>
2015-12-13 01:06:41 +0100

    Use debootstrap default variant to get networking

--- build.sh
+++ build.sh
@@ -5,19 +5,25 @@ set -x
 rm -fr debian-arm64
 
 # Debootstrap a minimal Debian Jessie rootfs 
-# --keyring /usr/share/keyrings/debian-ports-archive-keyring.gpg \
-# --no-check-gpg \
+#  --keyring /usr/share/keyrings/debian-ports-archive-keyring.gpg \
+#  --no-check-gpg \
+#  --variant=buildd \
 qemu-debootstrap \
   --arch=arm64 \
-  --variant=buildd \
   --exclude=debfoster \
   --include=net-tools \
   jessie \
   debian-arm64 \
   http://ftp.debian.org/debian
 
+### HypriotOS specific settings ###
+
+# set hostname to 'black-pearl'
+echo 'black-pearl' | sudo chroot debian-arm64 tee /etc/hostname
+
 # set root password to 'hypriot'
 echo 'root:hypriot' | sudo chroot debian-arm64 /usr/sbin/chpasswd
+
 
 # Package rootfs tarball
 tar -czf rootfs.tar.gz -C debian-arm64/ .
